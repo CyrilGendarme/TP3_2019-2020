9046dec0f68bb398c4b26f87f4b16871f934469c
Cyril Gendarme <cyrilgendarme123@gmail.com>
2020-04-22 18:36:47 +0200

    Class Article added

--- TP3_2019-2020/Objetcs/Article.cs
+++ TP3_2019-2020/Objetcs/Article.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Forms;
 
 namespace TP3_2019_2020.Objetcs
 {
@@ -13,9 +14,60 @@ namespace TP3_2019_2020.Objetcs
 
         public event PropertyChangedEventHandler PropertyChanged;
 
+        private List<Mot_clé> _listeMotClé;
+        public List<Mot_clé> ListeMotClé
+        {
+            get => _listeMotClé;
+            set
+            {
+                _listeMotClé = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private double _qualité; // valeur comprise entre 0 et 100
+        public double Qualité
+        {
+            get => _qualité;
+            set
+            {
+                if (value <= 100 && value >= 0) _qualité = value;
+                else
+                {
+                    DialogResult result;
+                    result = MessageBox.Show("La qualité doit être comprise entre 0 et 100", "Erreur de valeur", MessageBoxButtons.OK);
+                }
+            }
+        }
+
+        private List<String> _liensSortants;
+        public List<String> LiensSortants
+        {
+            get => _liensSortants;
+            set
+            {
+                _liensSortants = value;
+                NotifyPropertyChanged();
+            }
+        }
 
 
 
+
+
+        public Article() : base()
+        {
+            ListeMotClé = new List<Mot_clé>();
+            Qualité = 0;
+            LiensSortants = new List<String>();
+        }
+
+        public Article(String titre, List<Mot_clé> listMot, double quali, List<String> listLiens) : base(titre)
+        {
+            ListeMotClé = new List<Mot_clé>();
+            Qualité = quali;
+            LiensSortants = listLiens;
+        }
 
 
 
