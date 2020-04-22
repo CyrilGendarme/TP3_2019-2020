1e104779c2f271d1ec1ae56c754e7a7a665eab11
Cyril Gendarme <cyrilgendarme123@gmail.com>
2020-04-22 17:59:28 +0200

    Mot-clé Contenu added

--- TP3_2019-2020/Objetcs/Mot-clé.cs
+++ TP3_2019-2020/Objetcs/Mot-clé.cs
@@ -3,10 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace TP3_2019_2020.Objetcs
 {
-    class Mot_clé
+    public class Mot_clé
     {
+        public String Nom { get; set; }
+        public int Volume { get; set; }
+
+        private double _difficulté; // valeur comprise entre 0 et 100
+        public double Difficulté
+        {
+            get => _difficulté;
+            set
+            {
+                if (value <= 100 && value >= 0) _difficulté = value;
+                else
+                {
+                    DialogResult result;
+                    result = MessageBox.Show("La difficulté doit être comprise entre 0 et 100", "Erreur de valeur", MessageBoxButtons.OK);
+                }
+            }
+        }
+
+
+        public Mot_clé()
+        {
+            Nom = "NULL";
+            Volume = 0;
+            Difficulté = 100;
+        }
+
+        public Mot_clé(String s, int v, double d)
+        {
+            Nom = s;
+            Volume = v;
+            Difficulté = d;
+        }
+
+        public override String ToString()
+        {
+            return Nom;
+        }
     }
 }
---
+++ TP3_2019-2020/Objetcs/POI.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace TP1_2019_2020
+{
+    [Serializable]
+    public class POI : Coordonnees, ICartoObj, INotifyPropertyChanged
+    {
+        private String _desc;
+        public new event PropertyChangedEventHandler PropertyChanged;
+
+        public String Desc
+        {
+            get => _desc;
+            set
+            {
+                _desc = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public POI() : base(17.5, 20.87)         // values choosen to represent the HEPL site
+        {
+            Desc = "HEPL";
+        }
+        public POI(String desc, double lat, double longi) : base(lat, longi)
+        {
+            Desc = desc;
+        }
+
+
+
+        public override String ToString()
+        {
+            return base.ToString()  + "\tDescription = " + Desc; 
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyname = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+            }
+        }
+
+
+
+        public void SaveObjet()
+        {
+            BinaryFormatter binFormat = new BinaryFormatter();
+            using (Stream fstream = new FileStream(@"" + Desc + ".dat", FileMode.Append, FileAccess.Write, FileShare.None))
+            {
+                binFormat.Serialize(fstream, this);
+            }
+
+
+        }
+
+
+
+        //public void LoadObjet(String s)
+        //{
+        //    BinaryFormatter binFormat = new BinaryFormatter();
+        //    using (Stream fstream = new FileStream(s, FileMode.Open, FileAccess.Read, FileShare.None))
+        //    {
+        //        POI loaded = (POI)binFormat.Deserialize(fstream);
+        //        this.Desc = loaded.Desc;
+        //        this.Id = loaded.Id;
+        //        this.Latitude = loaded.Latitude;
+        //        this.Longitude = loaded.Latitude;
+        //    }
+
+        //}
+
+
+    }
+}
