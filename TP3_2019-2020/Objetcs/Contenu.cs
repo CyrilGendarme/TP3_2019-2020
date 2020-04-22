cc4007f7bdead685a2a610935abb07090c3c247a
Cyril Gendarme <cyrilgendarme123@gmail.com>
2020-04-22 17:47:43 +0200

    Class Contenu added

---
+++ TP3_2019-2020/Objetcs/Contenu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP3_2019_2020.Objetcs
+{
+    public class Contenu
+    {
+        public String Nom { get; set; }
+        public DateTime DateCreation { get; set; }
+
+
+
+        public Contenu()
+        {
+            Nom = "NULL";
+            DateCreation = DateTime.Now.Date;
+        }
+
+        public Contenu(String nom)
+        {
+            Nom = nom;
+            DateCreation = DateTime.Now.Date;
+        }
+
+
+    }
+}
