c75b6385ed6a3e46b40897e84b9c8cecc42c8335
Cyril Gendarme <cyrilgendarme123@gmail.com>
2020-04-26 09:56:29 +0200

    Création objet MyData

---
+++ TP3_2019-2020/Objetcs/MyData.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP3_2019_2020.Objetcs
+{
+
+    [Serializable]
+    class MyData : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<Thématique> _listThémattique;
+        public ObservableCollection<Collection> _listCollection;
+        public ObservableCollection<Produit> _listProduit;
+        public ObservableCollection<Article> _listArticle;
+        public ObservableCollection<Mot_clé> _listMotClé;
+
+
+
+
+
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyname = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyname));
+            }
+        }
+
+
+
+
+        public void SaveData()
+        {
+            BinaryFormatter binFormat = new BinaryFormatter();
+            using (Stream fstream = new FileStream(@"" + "MyWebsiteData.dat", FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                binFormat.Serialize(fstream, Data);
+            }
+
+
+        }
+
+        public void LoadData(String s)
+        {
+            BinaryFormatter binFormat = new BinaryFormatter();
+            using (Stream fstream = new FileStream(s, FileMode.Open, FileAccess.Read, FileShare.None))
+            {
+                Data = (ObservableCollection<ICartoObj>)binFormat.Deserialize(fstream);
+            }
+
+        }
+
+
+    }
+}
